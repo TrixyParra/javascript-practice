3c811f0fa8d969848e4f2d6768d6304c8ddcc846
TrixyParra <trixy.parra@gmail.com>
2022-05-17 00:11:31 -0700

    fixed CSS link on HTML. Added JS function to h1 - changes appearance - color, font size and new heading text.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function changeAppearance() {
+    element = document.querySelector('h1')  // to target same element - h1 // 
+    element.style.color = 'blue'
+    element.style.fontSize = '150px'
+    element.innerText = 'New Heading'
+}
